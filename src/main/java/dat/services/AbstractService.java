11f77591a25e2a001cc22ede54e52654fbbcb76f
73N37 <x0d4r4p@proton.me>
2025-10-11 21:38:20 +0200

    added readAllEntity() and readAllDTO()

--- src/main/java/dat/services/AbstractService.java
+++ src/main/java/dat/services/AbstractService.java
@@ -13,7 +13,38 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/*
+getDeclaredConstructor is part of Java's reflection API. You call in with this syntax "class.getDeclaredConstructor()" and it returns a java.lang.reflect.Constructor<T>.
+newInstance is a part of Java's reflection API. You call with this syntax "Constructor.newInstance()" and it returns an Object (instance of a class).
+
+NOTE:
+“Declared” means any visibility (public, protected, package, private) but only constructors declared in that class (constructors aren’t inherited anyway)
+
+Field is a part of Java's reflection API. A Field object is metadata + access handle for a single member field (instance or static) of a class.
+
+Field Introspect:
+- getName(): get the name of the variable as a String.
+- getAnnotation(Class<T> annotationClass): get a single annotation for a given Field.
+- getAnnotations(): get the annotations for a given Field (datatype is Annotation[]).
+- getType(): get a datatype
+- getSimpleName(): returns the package path of a class
+
+If a Field is not public you must execute command "field.setAccessible(true)" before assigning a Field to avoid access errors (IllegalArgumentException).
+If a Field is final (especially static final)assume the Field is immutable.
+Field Access:
+- get(Object target): receive the assigned value of a Field as an Object.
+- set(Object target, Object value): assign a value to a Field. Fx. set(String name, "73N37").
+
+Field[] is an array of the datatype Field. Arrays are covariant in Java (a Field[] is also an Object[]), so be careful not to write the wrong type through an Object[] alias (that would throw ArrayStoreException)
+Field[]:
+- class.getDeclaredFields(): Iterates through EVERY (public, protected, private, package) Field in a class and adds them to an Array (datatype is Field[]). Order is unspecified: don’t rely on array order being source order; sort if you care
+- class.getFields(): Iterates through ONLY public Fields in a class and adds them to an Array (datatype Field[]): Order is unspecified: don’t rely on array order being source order; sort if you care
+            */
 
 public abstract class AbstractService<  Entity  extends AbstractEntity,
                                         DTO     extends AbstractDTO,
@@ -21,8 +52,7 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
                                                 extends AbstractClass<   Entity, DTO, ID>
                                         implements      InterfaceService<Entity, DTO, ID>
 {
-    //protected InterfaceDAO<Entity, DTO, ID> dao = AbstractClass.dao;
-    private static final Logger logger = LoggerFactory.getLogger(AbstractService.class);
+    private static final Logger log = LoggerFactory.getLogger(AbstractService.class);
     protected final InterfaceDAO<Entity, DTO,ID> dao;
 
     public AbstractService(InterfaceDAO<Entity, DTO, ID>dao,
@@ -34,6 +64,27 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
         this.dao = dao;
     }
 
+    @Override
+    public Set<Entity> readAllEntity(){
+        // Uses Set<> on the readAll() method from DAO to remove all duplicate entries
+        log.debug("Retrieving all {} entities in database and removing duplicates", entityClass);
+        List<Entity> databaseEntries = dao.readAll();
+        return databaseEntries.
+                stream().
+                collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<DTO> readAllDTO(){
+        // Making my Set<Entity> into a Set<DTO>
+        log.debug("Transforming my Set<Entity> to a Set<DTO>", dtoClass);
+        Set<Entity> entitySet = readAllEntity();
+        return entitySet
+                .stream()
+                .map(this::entityToDTO)
+                .collect(Collectors.toSet());
+
+    }
 
     @Override
     public Entity dtoToEntity(DTO dto){
@@ -41,33 +92,10 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
             return null;
         }
         try {
-            /*
-                                                                                                                        getDeclaredConstructor is part of Java's reflection API. You call in with this syntax "class.getDeclaredConstructor()" and it returns a java.lang.reflect.Constructor<T>.
-                                                                                                                        newInstance is a part of Java's reflection API. You call with this syntax "Constructor.newInstance()" and it returns an Object (instance of a class).
 
-                                                                                                                        NOTE:
-                                                                                                                        “Declared” means any visibility (public, protected, package, private) but only constructors declared in that class (constructors aren’t inherited anyway)
-            */
             Entity entity = entityClass.getDeclaredConstructor().newInstance();
             /*
-                                                                                                                        Field is a part of Java's reflection API. A Field object is metadata + access handle for a single member field (instance or static) of a class.
-                                                                                                                        Field Introspect:
-                                                                                                                        - getName(): get the name of the variable as a String.
-                                                                                                                        - getAnnotation(Class<T> annotationClass): get a single annotation for a given Field.
-                                                                                                                        - getAnnotations(): get the annotations for a given Field (datatype is Annotation[]).
-                                                                                                                        - getType(): get a datatype
-                                                                                                                        - getSimpleName(): returns the package path of a class
 
-                                                                                                                        If a Field is not public you must execute command "field.setAccessible(true)" before assigning a Field to avoid access errors (IllegalArgumentException).
-                                                                                                                        If a Field is final (especially static final)assume the Field is immutable.
-                                                                                                                        Field Access:
-                                                                                                                        - get(Object target): receive the assigned value of a Field as an Object.
-                                                                                                                        - set(Object target, Object value): assign a value to a Field. Fx. set(String name, "73N37").
-
-                                                                                                                        Field[] is an array of the datatype Field. Arrays are covariant in Java (a Field[] is also an Object[]), so be careful not to write the wrong type through an Object[] alias (that would throw ArrayStoreException)
-                                                                                                                        Field[]:
-                                                                                                                        - class.getDeclaredFields(): Iterates through EVERY (public, protected, private, package) Field in a class and adds them to an Array (datatype is Field[]). Order is unspecified: don’t rely on array order being source order; sort if you care
-                                                                                                                        - class.getFields(): Iterates through ONLY public Fields in a class and adds them to an Array (datatype Field[]): Order is unspecified: don’t rely on array order being source order; sort if you care
             */
             Field[] dtoFields = dto.getClass().getDeclaredFields();
 
@@ -88,9 +116,9 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
                     entityField.setAccessible(true);
                     entityField.set(entity, value);
                 } catch (IllegalAccessException e) {
-                    logger.error("method failed since no matching method [newInstance()] was found");
+                    log.error("method failed since no matching method [newInstance()] was found");
                 } catch (NoSuchFieldException e){
-                    logger.debug("Field {} not found in entity {}", targetFieldName, entityClass.getSimpleName());
+                    log.debug("Field {} not found in entity {}", targetFieldName, entityClass.getSimpleName());
                 }
             }
             return entity;
@@ -133,9 +161,9 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
                     dtoField.setAccessible(true);
                     dtoField.set(dto, value);
                 } catch (IllegalAccessException e) {
-                    logger.error("method failed since no matching method [newInstance()] was found");
+                    log.error("method failed since no matching method [newInstance()] was found");
                 } catch (NoSuchFieldException e){
-                    logger.debug("Field {} not found in DTO {}", entityField.getName(), entityClass.getSimpleName());
+                    log.debug("Field {} not found in DTO {}", entityField.getName(), entityClass.getSimpleName());
                 }
             }
             return dto;
@@ -154,31 +182,31 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
     @Override
     public DTO create(DTO dto)
     {
-        logger.debug("Creating a new entry in database from a DTO = {}", dtoClass.getSimpleName());
+        log.debug("Creating a new entry in database from a DTO = {}", dtoClass.getSimpleName());
         if (dto == null){
-            logger.error("DTO is null");
+            log.error("DTO is null");
             throw new ApiException(ErrorTypes.BAD_REQUEST, "DTO is null");
         }
         try {
             Entity entity = dtoToEntity(dto);
             if(entity == null){
-                logger.error("Entity is null");
+                log.error("Entity is null");
                 throw new ApiException(ErrorTypes.BAD_REQUEST, "Failed to convert DTO to Entity");
             }
             Entity databaseEntry = dao.create(entity);
-            logger.info("Successfully add this {} as a new entry in the database ", databaseEntry.getClass().getSimpleName());;
+            log.info("Successfully add this {} as a new entry in the database ", databaseEntry.getClass().getSimpleName());;
             return entityToDTO(databaseEntry);
         } catch (ApiException e) {
             throw e;
         } catch (Exception e){
-            logger.error("Error while adding this {} as a new entry in the database", e.getMessage());
+            log.error("Error while adding this {} as a new entry in the database", e.getMessage());
             throw new ApiException(ErrorTypes.SERVER_ERROR, "Database error: " + e.getMessage());
         }
     }
 
     @Override
     public DTO read(ID id){
-        logger.debug("Fetching database entry with id {}", id);
+        log.debug("Fetching database entry with id {}", id);
         Optional<Entity> readEntry = dao.read(id);
         if (readEntry.isPresent()) {
             return entityToDTO(readEntry.get());
@@ -188,7 +216,7 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
 
     @Override
     public DTO update(ID id, DTO dto){
-        logger.debug("Updating database entry with id {}", id);
+        log.debug("Updating database entry with id {}", id);
         Optional<Entity> updateEntry = dao.update(id, dtoToEntity(dto));
         if(updateEntry.isPresent()){
             return entityToDTO(updateEntry.get());
@@ -198,7 +226,7 @@ public abstract class AbstractService<  Entity  extends AbstractEntity,
 
     @Override
     public void delete(ID id){
-        logger.debug("Deleting database entry with id {}", id);
+        log.debug("Deleting database entry with id {}", id);
         dao.delete(id);
     }
 }
