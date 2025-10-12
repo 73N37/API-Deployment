2f806d169f239efd0269cab5567acfc57d548bb4
73N37 <x0d4r4p@proton.me>
2025-10-12 02:04:58 +0200

    created a seperate EntityManagerFacotry for each Factory instance

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,8 +1,17 @@
 package dat;
 import dat.config.ApplicationConfig;
+import dat.config.HibernateConfig;
+import dat.dtos.HotelDTO;
+import dat.dtos.RoomDTO;
+import dat.entities.Hotel;
+import dat.entities.Room;
+import dat.factories.Factory;
+import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
+        new Factory(Hotel.class, HotelDTO.class, Integer.class, HibernateConfig.createEMF(false)).create();
+        new Factory(Room.class, RoomDTO.class, Integer.class, HibernateConfig.createEMF(false)).create();
         new ApplicationConfig().startServer(7070);
     }
 }
