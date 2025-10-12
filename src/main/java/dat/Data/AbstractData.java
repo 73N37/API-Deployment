a1d5601dd2130568e575152427ec2474bed05aa3
73N37 <x0d4r4p@proton.me>
2025-10-12 14:53:46 +0200

    Made comment more detailed

--- src/main/java/dat/Data/AbstractData.java
+++ src/main/java/dat/Data/AbstractData.java
@@ -3,6 +3,6 @@ package dat.Data;
 import dat.Unit.AbstractUnit;
 
 public abstract class AbstractData extends AbstractUnit {
-    // Shared data operations will go here
+    // Shared data operations (for entities & DTOs) will go here
     // getIdentifier() must be implemented by subclasses
 }
