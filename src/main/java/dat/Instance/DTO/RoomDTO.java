fb72111c27ab8f48adf5d425df598bb45ec53210
73N37 <x0d4r4p@proton.me>
2025-10-12 22:34:55 +0200

    uses getId() from dat.Instance.DTO.DTO

--- src/main/java/dat/Instance/DTO/RoomDTO.java
+++ src/main/java/dat/Instance/DTO/RoomDTO.java
@@ -22,11 +22,7 @@ public class RoomDTO extends dat.Instance.DTO.DTO {
         this.roomPrice = room.getRoomPrice().intValue();
         this.roomType = room.getRoomType();
     }
-
-//    public Integer getId(){
-//        return id;
-//    }
-
+    
     public static List<RoomDTO> toRoomDTOList(List<Room> rooms) {
         return List.of(rooms.stream().map(RoomDTO::new).toArray(RoomDTO[]::new));
     }
