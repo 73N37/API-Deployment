c1e81cc37a672c3395d24175d04eb33bba644f7f
73N37 <x0d4r4p@proton.me>
2025-10-12 22:29:20 +0200

    added Serializable ID & class extends Data

--- src/main/java/dat/Instance/DTO/DTO.java
+++ src/main/java/dat/Instance/DTO/DTO.java
@@ -1,4 +1,13 @@
 package dat.Instance.DTO;
 
-public class DTO {
+import dat.Instance.Data.Data;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class DTO <ID extends Serializable> extends Data {
+
+    public ID id;
+
 }
