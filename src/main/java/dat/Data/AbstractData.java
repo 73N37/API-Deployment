e8e611db867d305fba08f6c3dcfe062146f9bb58
73N37 <x0d4r4p@proton.me>
2025-10-12 14:52:47 +0200

    Created a sub-class to AbstractUnit. This class is which all entities & all DTOs inherit from

--- src/main/java/dat/Data/AbstractData.java
+++ src/main/java/dat/Data/AbstractData.java
@@ -1,4 +1,8 @@
 package dat.Data;
 
-public class AbstractData {
+import dat.Unit.AbstractUnit;
+
+public abstract class AbstractData extends AbstractUnit {
+    // Shared data operations will go here
+    // getIdentifier() must be implemented by subclasses
 }
