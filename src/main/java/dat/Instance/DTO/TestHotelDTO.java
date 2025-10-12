06bfc8f25385e6f5728d2d49b3a845237da58091
73N37 <x0d4r4p@proton.me>
2025-10-13 01:16:33 +0200

    added a Constructor [public TestHotelDTO(Hotel)]

--- src/main/java/dat/Instance/DTO/TestHotelDTO.java
+++ src/main/java/dat/Instance/DTO/TestHotelDTO.java
@@ -1,4 +1,26 @@
 package dat.Instance.DTO;
 
-public class TestHotel {
+import dat.Annotation.IgnoreMapping;
+import dat.Annotation.MapTo;
+import dat.Instance.Entity.Hotel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestHotelDTO extends dat.Blueprint.DTO.AbstractDTO<Integer>
+{
+    @MapTo("hotelName")
+    private String hotelName;   // Maps to entity's "hotelName" Field
+    private String hotelAddress;
+    @IgnoreMapping
+    private Hotel.HotelType hotelType; // TODO: Will be ignored by "dtoToEntity" method due to @IgnoreMapping
+    private Set<RoomDTO> rooms = new HashSet<>();
+
+    public TestHotelDTO(Hotel hotel){
+        super(hotel.getId());
+        this.hotelName = hotel.getHotelName();
+        this.hotelAddress = hotel.getHotelAddress();
+        this.hotelType = hotel.getHotelType();
+        // this.rooms = hotel.getRooms();
+    }
 }
