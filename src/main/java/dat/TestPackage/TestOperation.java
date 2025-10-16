0e26ce424f35357e5a87741dd06be7fce65dfd8b
73N37 <x0d4r4p@proton.me>
2025-10-16 16:43:31 +0200

    Added DAO, Service, Controller

--- src/main/java/dat/TestPackage/TestOperation.java
+++ src/main/java/dat/TestPackage/TestOperation.java
@@ -6,14 +6,11 @@ package dat.TestPackage;
  */
 
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.NoResultException;
-import jakarta.persistence.TypedQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class TestDAO {
+public final class TestOperation {
     /*      TODO:   HVIS DU VIL HAVE 12, SÅ SKAL DU LAVE TESTS AF ALLE DINE KLASSER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             TODO:   HVIS DU VIL HAVE 12, SÅ SKAL DU LAVE TESTS AF ALLE DINE KLASSER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             TODO:   HVIS DU VIL HAVE 12, SÅ SKAL DU LAVE TESTS AF ALLE DINE KLASSER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -41,28 +38,42 @@ public final class TestDAO {
  */
 
 
-        private static final Logger log = LoggerFactory.getLogger(dat.TestPackage.TestDAO.class);
+        private static final Logger log = LoggerFactory.getLogger(TestOperation.class);
         public EntityManagerFactory emf;
-        Object entityClass = dat.TestPackage.TestData.Methods.getEntity();
         //lombok.var test
 
+    static class DAO{
+
+
+         interface Interface<Entity, ID>{
+            Entity read(ID id);
+            java.util.List<Entity> readAll();
+            Entity create(Entity entity);
+            Entity update(Entity entity, ID id);
+            void delete(ID id);
+            boolean validatePrimaryKey(ID id);
+        }
+    }
+
+    static class Service{}
+    static class Controller{}
+    static class Routes{}
 
 
 
-
-    private static class ApiException extends RuntimeException {
+    static class ApiException extends RuntimeException {
         private final int code;
-        private final dat.TestPackage.TestDAO.TestErrorTypes errorType;
+        private final TestOperation.TestErrorTypes errorType;
         private static String errorMsg;
 
         public ApiException(int code, String msg) {
             super(msg);
             this.code = code;
-            this.errorType = dat.TestPackage.TestDAO.TestErrorTypes.getType(code);
+            this.errorType = TestOperation.TestErrorTypes.getType(code);
             this.errorMsg = errorType.getErrorMessage() + "\n";
         }
 
-        public ApiException(dat.TestPackage.TestDAO.TestErrorTypes errorType, String msg) {
+        public ApiException(TestOperation.TestErrorTypes errorType, String msg) {
             super(msg);
             this.errorType = errorType;
             this.code = errorType.getErrorCode();
@@ -75,49 +86,49 @@ public final class TestDAO {
     */
 
         //TODO: 400 bad request
-        public static dat.TestPackage.TestDAO.ApiException badRequest (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.BAD_REQUEST, msg + errorMsg);
+        public static TestOperation.ApiException badRequest (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.BAD_REQUEST, msg + errorMsg);
         }
 
         //TODO: 401 Unauthorized
-        public static dat.TestPackage.TestDAO.ApiException unauthorized (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.UNAUTHORIZED, msg + errorMsg);
+        public static TestOperation.ApiException unauthorized (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.UNAUTHORIZED, msg + errorMsg);
         }
 
         //TODO: 403 Forbidden Access
-        public static dat.TestPackage.TestDAO.ApiException forbidden (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.FORBIDDEN, msg + errorMsg);
+        public static TestOperation.ApiException forbidden (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.FORBIDDEN, msg + errorMsg);
         }
 
         //TODO: 404 not found
-        public static dat.TestPackage.TestDAO.ApiException notFound(String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.NOT_FOUND, msg + errorMsg);
+        public static TestOperation.ApiException notFound(String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.NOT_FOUND, msg + errorMsg);
         }
 
         //TODO: 405 conflict
-        public static dat.TestPackage.TestDAO.ApiException conflict (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.METHOD_NOT_ALLOWED, msg + errorMsg);
+        public static TestOperation.ApiException conflict (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.METHOD_NOT_ALLOWED, msg + errorMsg);
         }
 
         //TODO: 406 Not Acceptable
-        public static dat.TestPackage.TestDAO.ApiException notAcceptable (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.NOT_ACCEPTABLE, msg + errorMsg);
+        public static TestOperation.ApiException notAcceptable (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.NOT_ACCEPTABLE, msg + errorMsg);
         }
 
         //TODO: 409 already exists
-        public static dat.TestPackage.TestDAO.ApiException alreadyExists(String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.ALREADY_EXISTS, msg + errorMsg);
+        public static TestOperation.ApiException alreadyExists(String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.ALREADY_EXISTS, msg + errorMsg);
 
         }
 
         //TODO: 413 Payload too large
-        public static dat.TestPackage.TestDAO.ApiException payloadTooLarge(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.PAYLOAD_TOO_LARGE, msg + errorMsg);
+        public static TestOperation.ApiException payloadTooLarge(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.PAYLOAD_TOO_LARGE, msg + errorMsg);
         }
 
         //TODO: 429 Too many requests
-        public static dat.TestPackage.TestDAO.ApiException tooManyRequests(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.TOO_MANY_REQUESTS, msg + errorMsg);
+        public static TestOperation.ApiException tooManyRequests(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.TOO_MANY_REQUESTS, msg + errorMsg);
         }
 
     /* |----------------------|
@@ -126,58 +137,58 @@ public final class TestDAO {
     */
 
         //TODO: 500 server error
-        public static dat.TestPackage.TestDAO.ApiException serverError (String msg) {
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.SERVER_ERROR, msg + errorMsg);
+        public static TestOperation.ApiException serverError (String msg) {
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.SERVER_ERROR, msg + errorMsg);
         }
 
         //TODO: 501 Not implemented
-        public static dat.TestPackage.TestDAO.ApiException notImplemented(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.NOT_IMPLEMENTED, msg + errorMsg);
+        public static TestOperation.ApiException notImplemented(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.NOT_IMPLEMENTED, msg + errorMsg);
         }
 
         //TODO: 502 Bad Gateway
-        public static dat.TestPackage.TestDAO.ApiException badGateway(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.BAD_GATEWAY, msg + errorMsg);
+        public static TestOperation.ApiException badGateway(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.BAD_GATEWAY, msg + errorMsg);
         }
 
         //TODO: 503 Service Unavailable
-        public static dat.TestPackage.TestDAO.ApiException serviceUnavailable(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.SERVICE_UNAVAILABLE, msg + errorMsg);
+        public static TestOperation.ApiException serviceUnavailable(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.SERVICE_UNAVAILABLE, msg + errorMsg);
         }
 
         //TODO: 504 Gateway Timeout
-        public static dat.TestPackage.TestDAO.ApiException gatewayTimeout(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.GATEWAY_TIMEOUT, msg + errorMsg);
+        public static TestOperation.ApiException gatewayTimeout(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.GATEWAY_TIMEOUT, msg + errorMsg);
         }
 
         //TODO: 505 HTTP Version not supported
-        public static dat.TestPackage.TestDAO.ApiException versionNotSupported(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.HTTP_VERSION_NOT_SUPPORTED, msg + errorMsg);
+        public static TestOperation.ApiException versionNotSupported(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.HTTP_VERSION_NOT_SUPPORTED, msg + errorMsg);
         }
 
         //TODO: 506 Variant also negotiates
-        public static dat.TestPackage.TestDAO.ApiException variantNegotiates(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.VARIANT_ALSO_NEGOTIATES, msg + errorMsg);
+        public static TestOperation.ApiException variantNegotiates(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.VARIANT_ALSO_NEGOTIATES, msg + errorMsg);
         }
 
         //TODO: 507 Insufficient storage
-        public static dat.TestPackage.TestDAO.ApiException insufficientStorage(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.INSUFFICIENT_STORAGE, msg + errorMsg);
+        public static TestOperation.ApiException insufficientStorage(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.INSUFFICIENT_STORAGE, msg + errorMsg);
         }
 
         //TODO: 508 Loop detected
-        public static dat.TestPackage.TestDAO.ApiException loopDetected(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.LOOP_DETECTED, msg + errorMsg);
+        public static TestOperation.ApiException loopDetected(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.LOOP_DETECTED, msg + errorMsg);
         }
 
         //TODO: 510 Not extended
-        public static dat.TestPackage.TestDAO.ApiException notExtended(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.NOT_EXTENDED, msg + errorMsg);
+        public static TestOperation.ApiException notExtended(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.NOT_EXTENDED, msg + errorMsg);
         }
 
         //TODO: 511 Network Authentication Required
-        public static dat.TestPackage.TestDAO.ApiException authenticationRequired(String msg){
-            return new dat.TestPackage.TestDAO.ApiException(dat.TestPackage.TestDAO.TestErrorTypes.NETWORK_AUTHENTICATION_REQUIRED, msg + errorMsg);
+        public static TestOperation.ApiException authenticationRequired(String msg){
+            return new TestOperation.ApiException(TestOperation.TestErrorTypes.NETWORK_AUTHENTICATION_REQUIRED, msg + errorMsg);
         }
 
         public int getStatusCode() {
@@ -185,7 +196,7 @@ public final class TestDAO {
         }
     }
 
-    public enum TestErrorTypes {
+    enum TestErrorTypes {
         BAD_REQUEST(400, "Bad request"),
         UNAUTHORIZED(401, "Unauthorized Access"),
         FORBIDDEN(403, "Forbidden Access"),
@@ -220,8 +231,8 @@ public final class TestDAO {
         }
 
 
-        public static dat.TestPackage.TestDAO.TestErrorTypes getType(int errorCode) {
-            for (dat.TestPackage.TestDAO.TestErrorTypes type : dat.TestPackage.TestDAO.TestErrorTypes.values()) {
+        public static TestOperation.TestErrorTypes getType(int errorCode) {
+            for (TestOperation.TestErrorTypes type : TestOperation.TestErrorTypes.values()) {
                 if (type.getErrorCode() == errorCode) {
                     return type;
                 }
