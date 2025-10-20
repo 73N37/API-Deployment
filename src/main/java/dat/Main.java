14b4f49d907071ffdb84a06b2f38b6a091919dc6
73N37 <x0d4r4p@proton.me>
2025-10-20 10:40:23 +0200

    began to use var

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -24,12 +24,13 @@ public class Main
         }));
 
         app.start(7070);
+        var dto = new TestInformation.Data().putDTO("Through hard work comes EXCELLENCE");
+        var entity = new TestInformation.Data().putEntity( "Merovingian");
 
-
-        System.out.println("DTO Data = " + new TestInformation.Data().putDTO("Through hard work comes EXCELLENCE"));
-        System.out.println("Entity Data = " + new TestInformation.Data().putEntity( "Merovingian"));
-        System.out.println("globalEntity Name = {Merovingian} = " + new TestInformation.Data().getEntity());
-        System.out.println("globalDTO Name {Through hard work comes EXCELLENCE}, " + new TestInformation.Data().getDTO());
+        System.out.println("DTO Data = " + dto);
+        System.out.println("Entity Data = " + entity);
+        System.out.println("globalEntity Name = {Merovingian} = " + new dat.TestPackage.TestInformation.Data().getName(entity));
+        System.out.println("globalDTO Name {Through hard work comes EXCELLENCE}, " + new dat.TestPackage.TestInformation.Data().getName(dto));
         System.out.println("=== MAIN IS COMPLETE ===");
     }
 }
