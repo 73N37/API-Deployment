769c445be17666655c25b723de8e445cb09e5e0a
73N37 <x0d4r4p@proton.me>
2025-10-20 12:46:07 +0200

    Added (protected) role enum, so both Information and Operation can use the same reference point

--- src/main/java/dat/TestPackage/TestUtilization.java
+++ src/main/java/dat/TestPackage/TestUtilization.java
@@ -2,4 +2,14 @@ package dat.TestPackage;
 
 public class TestUtilization {
     protected static final jakarta.persistence.EntityManagerFactory                     emf = dat.Config.HibernateConfig.createEMF(false);  // I changed this to be created on instantiation of this class. So that I do not need to pass it around all the time.
+
+
+    protected enum
+    Role implements io.javalin.security.RouteRole
+    {
+        ANYONE,
+        USER,
+        ADMIN,
+        MODERATOR;
+    }
 }
