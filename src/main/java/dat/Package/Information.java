40838b614820982feea698f2b26684382cd740f0
73N37 <x0d4r4p@proton.me>
2025-10-21 12:33:44 +0200

    I will become better at making individuel commits

--- src/main/java/dat/Package/Information.java
+++ src/main/java/dat/Package/Information.java
@@ -27,26 +27,15 @@ package dat.Package;
  * @version 1.0
  * @since 2025-10-15
  */
-public final class Information extends dat.Package.Utilization
+public class Information extends dat.Package.Utilization
 {   // Information [class] begins
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Information.class);
 
     private static final java.util.concurrent.atomic.AtomicInteger idCounter = new java.util.concurrent.atomic.AtomicInteger(0);
     private static dat.Package.Information.Entity   globalEntity;
     private static dat.Package.Information.DTO      globalDTO;
-    private static dat.Package.Information          instance;
 
-    private Information(){}
-
-    private static dat.Package.Information
-    get()
-    {   // get() [method] begins
-        if(instance == null)
-        {
-            instance = new Information();
-        }
-        return instance;
-    }   // get() [method] ends
+    protected Information(){}
 
 
     public static class
@@ -57,6 +46,7 @@ public final class Information extends dat.Package.Utilization
         getEntityClass() {
             return dat.Package.Information.Entity.class;
         }
+
 
         public java.lang.Integer
         getId
@@ -109,24 +99,30 @@ public final class Information extends dat.Package.Utilization
             globalDTO = null;
         }
 
-        public dat.Package.Information
-        getInstance
-                (
-                        dat.Package.Utilization.Role role
-                )
-        {   // getInstance(Role) [method] begins
-//            return dat.Package.Information.get();
-            try {   // try-block [conditional] begins
-                log.debug("Checking user access");
-                boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.ANYONE.get());
-                log.debug("Access was GRANTED");
-                return (accept) ? dat.Package.Information.get() : null;
-            }   // try-block [conditional] ends
-            catch (Exception e) {   // catch-block [conditional] begins
-                log.error("An error happen while either granting user access or returning the instance of {}", dat.Package.Information.class, e);
-                return null;
-            }   // catch-block [conditional] ends
-        }   // getInstance(Role) [method] begins
+        public dat.Package.Utilization
+        getInstance()
+        {
+            return dat.Package.Utilization.getInstance();
+        }
+
+//        public dat.Package.Information
+//        getInstance
+//                (
+//                        dat.Package.Utilization.Role role
+//                )
+//        {   // getInstance(Role) [method] begins
+////            return dat.Package.Information.get();
+//            try {   // try-block [conditional] begins
+//                log.debug("Checking user access");
+//                boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.ANYONE.get());
+//                log.debug("Access was GRANTED");
+//                return (accept) ? this.get() : null;
+//            }   // try-block [conditional] ends
+//            catch (Exception e) {   // catch-block [conditional] begins
+//                log.error("An error happen while either granting user access or returning the instance of {}", dat.Package.Information.class, e);
+//                return null;
+//            }   // catch-block [conditional] ends
+//        }   // getInstance(Role) [method] begins
 
         public dat.Package.Information.DTO
         getDTO(dat.Package.Utilization.Role role) {   // getDTO(Role) [method] begins
@@ -142,63 +138,66 @@ public final class Information extends dat.Package.Utilization
             }   // catch [conditional] ends
         }   // getDTO(Role) [method] ends
 
-
-        public dat.Package.Information.Entity
-        get
-                (
-                        java.lang.Integer id,
-                        dat.Package.Utilization.Role role
-                ) {
-            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.USER.get());
-            return (accept) ? DAO.get(id) : null;
-        }
-
-        public dat.Package.Information.Entity
-        put
-                (
-                        java.lang.String name,
-                        dat.Package.Utilization.Role role
-                ) {
-            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.USER.get());
-            return (accept) ? DAO.put(name) : null;
-        }
-
-        public void
-        delete
-                (
-                        dat.Package.Information.Entity entity,
-                        dat.Package.Utilization.Role role
-
-                ) {
-            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
-            if (accept) {
-                DAO.delete(entity);
-            }
-        }
-
-
-        public void
-        delete
-                (
-                        java.lang.Integer id,
-                        dat.Package.Utilization.Role role
-                ) {
-            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
-            if (accept) {
-                DAO.delete(id);
-            }
-        }
-
-        public dat.Package.Information.Entity
-        patch
-                (
-                        dat.Package.Information.Entity entity,
-                        dat.Package.Utilization.Role role
-
-                ) {
-            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
-            return (accept) ? DAO.patch(entity) : null;
-        }
+//
+//        public dat.Package.Information.Entity
+//        get
+//                (
+//                        java.lang.Integer id,
+//                        dat.Package.Utilization.Role role
+//                ) {
+//            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.USER.get());
+//            if (accept)
+//            {
+//                var super.dao.get(id);
+//            }
+//        }
+//
+//        public dat.Package.Information.Entity
+//        put
+//                (
+//                        java.lang.String name,
+//                        dat.Package.Utilization.Role role
+//                ) {
+//            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.USER.get());
+//            return (accept) ? Operation.DAO.put(name) : null;
+//        }
+//
+//        public void
+//        delete
+//                (
+//                        dat.Package.Information.Entity entity,
+//                        dat.Package.Utilization.Role role
+//
+//                ) {
+//            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
+//            if (accept) {
+//                DAO.delete(entity);
+//            }
+//        }
+//
+//
+//        public void
+//        delete
+//                (
+//                        java.lang.Integer id,
+//                        dat.Package.Utilization.Role role
+//                ) {
+//            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
+//            if (accept) {
+//                DAO.delete(id);
+//            }
+//        }
+//
+//        public dat.Package.Information.Entity
+//        patch
+//                (
+//                        dat.Package.Information.Entity entity,
+//                        dat.Package.Utilization.Role role
+//
+//                ) {
+//            boolean accept = dat.Package.Utilization.Role.isAccessAllowed(role.get(), dat.Package.Utilization.Role.MODERATOR.get());
+//            return (accept) ? DAO.patch(entity) : null;
+//        }
 
 //        public java.lang.String
 //        getName
@@ -250,7 +249,7 @@ public final class Information extends dat.Package.Utilization
     }
 
     @jakarta.persistence.Entity
-    private static class
+    static class
     Entity extends DataUnit
     {   // Entity [class] begins
         @jakarta.persistence.Id
@@ -299,7 +298,7 @@ public final class Information extends dat.Package.Utilization
         }
     }   // Entity [class] ends
 
-    private static class
+    static class
     DTO extends DataUnit
     {   // DTO [class] begins
         private DTO
@@ -323,103 +322,103 @@ public final class Information extends dat.Package.Utilization
         }
     }   // DTO [class] ends
 
-    private static class
-    DAO
-    {
-        private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DAO.class);
-        protected static jakarta.persistence.EntityManagerFactory emf = dat.Config.HibernateConfig.createEMF(true);
-        private static dat.Package.Information.Entity
-        put
-                (
-                        java.lang.String name
-                )
-        {
-            dat.Package.Information.Entity entity = null;
-            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager())
-            {
-                log.debug("Attempting to persist and entity");
-                em.getTransaction().begin();
-                entity = new dat.Package.Information.Entity(name);
-                em.persist(entity);
-                em.getTransaction().commit();
-                log.debug("Successfully persisted Entity={}", entity);
-                return entity;
-            } catch (Exception e){
-                log.error("An error happen while attempting to persist entity={}", entity, e);
-                return null;
-            }
-        }
-
-        private static dat.Package.Information.Entity
-        get
-                (
-                        java.lang.Integer id
-                )
-        {
-            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()) {
-                log.debug("Attempting to find an entity by ID");
-                em.getTransaction().begin();
-                dat.Package.Information.Entity result = em.find(dat.Package.Information.Entity.class, id);
-                em.getTransaction().commit();
-                log.debug("Successfully found an entity by ID ");
-                return result;
-            } catch (Exception e)
-            {
-                log.error("Was unable to find an entity by this ID={}", id, e);
-                return null;
-            }
-        }
-
-        private static void
-        delete
-                (
-                        dat.Package.Information.Entity entity
-                )
-        {
-
-            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()){
-                log.debug("Attempting to delete this Entity={}", entity);
-                em.getTransaction().begin();
-                em.remove(entity);
-                em.getTransaction().commit();
-            } catch (Exception e){
-                log.error("An error happen while trying to delete this Entity={}", entity, e);
-            }
-        }
-
-        private static void
-        delete
-                (
-                        java.lang.Integer id
-                )
-        {
-            try{
-                log.debug("Attempting to to delete an Entity by ID={}", id);
-                dat.Package.Information.Entity entity = DAO.get(id);
-                DAO.delete(entity);
-                log.debug("Successfully delete Entity=", entity);
-            } catch (Exception e){
-                log.error("An error happen while trying to delete an Entity with ID=", id,e);
-            }
-        }
-
-        private static dat.Package.Information.Entity
-        patch
-                (
-                        dat.Package.Information.Entity entity
-                )
-        {
-
-            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()){
-                log.debug("Attempting to patch (merge) entity={}", entity);
-                em.getTransaction().begin();
-                dat.Package.Information.Entity result = em.merge(entity);
-                em.getTransaction().commit();
-                return result;
-            } catch (Exception e){
-                log.error("was unable to merge entity={}", entity,e);
-                return null;
-            }
-        }
-    }
+//    private static class
+//    DAO
+//    {   // TODO EVERYTHING NEEDS BE IN OPERATION CLASS
+//        private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DAO.class);
+//
+//        private static dat.Package.Information.Entity
+//        put
+//                (
+//                        java.lang.String name
+//                )
+//        {
+//            dat.Package.Information.Entity entity = null;
+//            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager())
+//            {
+//                log.debug("Attempting to persist and entity");
+//                em.getTransaction().begin();
+//                entity = new dat.Package.Information.Entity(name);
+//                em.persist(entity);
+//                em.getTransaction().commit();
+//                log.debug("Successfully persisted Entity={}", entity);
+//                return entity;
+//            } catch (Exception e){
+//                log.error("An error happen while attempting to persist entity={}", entity, e);
+//                return null;
+//            }
+//        }
+//
+//        private static dat.Package.Information.Entity
+//        get
+//                (
+//                        java.lang.Integer id
+//                )
+//        {
+//            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()) {
+//                log.debug("Attempting to find an entity by ID");
+//                em.getTransaction().begin();
+//                dat.Package.Information.Entity result = em.find(dat.Package.Information.Entity.class, id);
+//                em.getTransaction().commit();
+//                log.debug("Successfully found an entity by ID ");
+//                return result;
+//            } catch (Exception e)
+//            {
+//                log.error("Was unable to find an entity by this ID={}", id, e);
+//                return null;
+//            }
+//        }
+//
+//        private static void
+//        delete
+//                (
+//                        dat.Package.Information.Entity entity
+//                )
+//        {
+//
+//            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()){
+//                log.debug("Attempting to delete this Entity={}", entity);
+//                em.getTransaction().begin();
+//                em.remove(entity);
+//                em.getTransaction().commit();
+//            } catch (Exception e){
+//                log.error("An error happen while trying to delete this Entity={}", entity, e);
+//            }
+//        }
+//
+//        private static void
+//        delete
+//                (
+//                        java.lang.Integer id
+//                )
+//        {
+//            try{
+//                log.debug("Attempting to to delete an Entity by ID={}", id);
+//                dat.Package.Information.Entity entity = DAO.get(id);
+//                DAO.delete(entity);
+//                log.debug("Successfully delete Entity=", entity);
+//            } catch (Exception e){
+//                log.error("An error happen while trying to delete an Entity with ID=", id,e);
+//            }
+//        }
+//
+//        private static dat.Package.Information.Entity
+//        patch
+//                (
+//                        dat.Package.Information.Entity entity
+//                )
+//        {
+//
+//            try (jakarta.persistence.EntityManager em = dat.Package.Information.DAO.emf.createEntityManager()){
+//                log.debug("Attempting to patch (merge) entity={}", entity);
+//                em.getTransaction().begin();
+//                dat.Package.Information.Entity result = em.merge(entity);
+//                em.getTransaction().commit();
+//                return result;
+//            } catch (Exception e){
+//                log.error("was unable to merge entity={}", entity,e);
+//                return null;
+//            }
+//        }
+//    }
 }   //  Information [class] ends
